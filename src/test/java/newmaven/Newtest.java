96f98b12084725e4be6090042515d3a9fd4abe59
Manoj Kumar Nelakurthy <manojkumarnelakurthy@gmail.com>
2021-05-05 16:26:55 +0530

    hi

--- src/test/java/newmaven/Newtest.java
+++ src/test/java/newmaven/Newtest.java
@@ -1,6 +1,5 @@
 package newmaven;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeTest;
